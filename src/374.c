4b6eb7841f1a10d7b1d8e791ed9195f3789d92af
iori <wust.ajax@gmail.com>
2013-07-06 10:09:42 +0800

    uva374, 测试不通过。 (a*b)%m = (a%m*b)%m
    2374859
    3029382
    36123
    ans: -29255

---
+++ src/374.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+#define OJ_TEST 0
+#define LOCAL_TEST 1
+#define M 0
+
+static void solve(void){
+	int b, p, m;
+	int i, t;
+	while(~scanf("%d%d%d", &b, &p, &m)){
+		for(t=1, i=0; i<p; ++i){
+			t *= b;
+			t %= m;
+		}
+		printf("%d\n", t);
+	}
+}
+#if OJ_TEST
+	int main(int ac, char *av[]){
+		solve();
+		return 0;
+	}
+#elif  LOCAL_TEST
+	void run(void){
+		solve();
+	}
+
+#else
+
+#endif
