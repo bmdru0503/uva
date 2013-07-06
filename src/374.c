f9fd759843094aa0d29bc79fb77f0b94d815aac4
iori <wust.ajax@gmail.com>
2013-07-06 10:52:46 +0800

    uva374 补充： 关于 a^b的解法 3^5=3^(101)2 =3^4*3^1, 可以计算出 3^1, 3^2, 3^4,3^8获取指数的2进制表示，当某一位是1时，并入结果。

--- src/374.c
+++ src/374.c
@@ -4,18 +4,18 @@
 #include <math.h>
 #include <limits.h>
 #define OJ_TEST 0
-#define LOCAL_TEST 1
+#define LOCAL_TEST 0
 #define M 0
 
 static void solve(void){
 	int b, p, m, i;
 	long long t, s;
 	while(~scanf("%d%d%d", &b, &p, &m)){
-		t=1;
+		t=1; /* 最终结果 */
 		s=b;
 		while(p>=1){
 			if(p%2) t = t*s%m;
-			s = s*s%m;
+			s = s*s%m;  /* s为 b^(2^i) %m的值  */
 			p /= 2;
 		}
 		printf("%d\n", (int)t);
