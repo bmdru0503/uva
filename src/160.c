9d3c738ffa9e35aad1a5d51812254af955db9702
iori <wust.ajax@gmail.com>
2013-07-06 14:32:04 +0800

    uva160, wa, ??? 思路： 阶乘的因子分解表示法，注意格式。

---
+++ src/160.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+#define OJ_TEST 0
+#define LOCAL_TEST 1
+#define M 101
+
+static void solve(void){
+	int primes[M];
+	int temp[M], i, j, k;
+	int n;
+
+	memset(temp, -1, sizeof(temp));
+	for(k=0,i=2; i<M; ++i){
+		if(!temp[i]) continue;
+		primes[k++] = i;
+		for(j=i*i; j<M; j +=i){
+			temp[j] = 0;
+		}
+	}
+	while(scanf("%d", &n), n){
+		memset(temp, 0, sizeof(temp));
+		for(i=2; i<=n; ++i){
+			for(j=i, k=0; j>1; ++k){
+				while(j%primes[k]==0){
+					j /=primes[k];
+					++temp[k];
+				}
+			}
+		}
+		printf("%3d! =", n);
+		for(i=0; primes[i]<=n; ++i){
+			if(i&& (i%15==0)){
+				printf("\n      "); /* six space  %3d!-  */
+			}
+			printf("%3d", temp[i]);
+		}
+		printf("\n");
+	}
+}
+#if OJ_TEST
+	int main(int ac, char *av[]){
+		solve();
+		return 0;
+	}
+#elif  LOCAL_TEST
+	void run(void){
+		solve();
+	}
+
+#else
+
+#endif
