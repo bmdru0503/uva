7c1e71dc77bd45f167911424e19b08801ff59f15
iori <wust.ajax@gmail.com>
2013-07-08 15:10:58 +0800

    uva10469, ac, 两个 无符号数的异或运算。

---
+++ src/10469.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+#define OJ_TEST 0
+#define LOCAL_TEST 0
+#define M 0
+
+static void solve(void){
+	unsigned a, b;
+	while(~scanf("%u%u", &a, &b)){
+		printf("%u\n", a^b);
+	}
+}
+#if OJ_TEST
+	int main(int ac, char *av[]){
+		solve();
+		return 0;
+	}
+#elif  LOCAL_TEST
+	void run(void){
+		solve();
+	}
+
+#else
+
+#endif
