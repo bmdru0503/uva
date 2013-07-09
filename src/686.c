1525248c2b606edb5d161a513938266559c2877e
iori <wust.ajax@gmail.com>
2013-07-09 08:52:51 +0800

    uva686, 少许改进。

--- src/686.c
+++ src/686.c
@@ -8,13 +8,15 @@
 #define M ((1<<15)+1)
 
 static int prime[M];
-
+static int seq[M];
 void seize(void){
-	int i,j;
+	int i,j, k;
 	memset(prime, -1, sizeof(prime));
 	for(i=4; i<M; i+=2) prime[i]=0;
-	for(i=3; i<M; i+=2){
+	seq[0]=2;
+	for(k=1,i=3; i<M; i+=2){
 		if(prime[i]){
+			seq[k++] = i;
 			for(j=i*i; j<M; j+=i){
 				prime[j] = 0;
 			}
@@ -25,8 +27,8 @@ static void solve(void){
 	int n, i, ans;
 	seize();
 	while(scanf("%d", &n), n){
-		for(ans=0, i=2; i<=n/2; ++i){
-			if(prime[i] && prime[n-i]) ++ans;
+		for(ans=0, i=0; seq[i]<=n/2; ++i){
+			if(prime[n-seq[i]]) ++ans;
 		}
 		printf("%d\n", ans);
 	}
