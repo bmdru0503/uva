ed3d66f55f8a136d0018c65a05eda60342aa5846
iori <wust.ajax@gmail.com>
2013-07-11 18:33:59 +0800

    uva10003 - Cutting Sticks, ac dp, 和矩阵连乘问题差不多。

--- src/10003.c
+++ src/10003.c
@@ -4,33 +4,32 @@
 #include <math.h>
 #include <limits.h>
 #define OJ_TEST 0
-#define LOCAL_TEST 1
-#define M 10001
-static int seg[M];
+#define LOCAL_TEST 0
+#define M 52
+static int cuts[M][M];
+static int pos[M];
 
 static void solve(void){
-	int end, c1, c2, n, i, k, ms, mt, ans;
+	int end, n, i, j, k, ms, mt;
 	while(scanf("%d", &end), end){
 		scanf("%d", &n);
-		for(c1=0, i=0; i<n; ++i){
-			scanf("%d", &c2);
-			seg[i] = c2-c1;
-			c1 = c2;
+		pos[0] = 0;
+		for(i=1; i<=n; ++i){
+			scanf("%d", &pos[i]);
 		}
-		seg[i] = end - c2;
-		for(ans=0, c1=0, c2=n+1; c1<n; ++c1){
-			for(ms=2000, i=0; i<c2-1; ++i){
-				mt = seg[i]+seg[i+1];
-				if(mt<ms){
-					ms = mt; k=i;
+		pos[++n] = end;
+		for(i=0; i<n; ++i) cuts[i][i+1] = 0;
+		for(k=1; k<n; ++k){ /* 两切割点中间切割点的数量 ,0和n为两端，其实未害割 */
+			for(i=0; i<=n-k; ++i){
+				for(ms=0x7fffffff, j=i+1; j<k+i+1; ++j){
+					mt = cuts[i][j]+cuts[j][i+k+1] + pos[i+k+1]-pos[i];
+					if(mt<ms) ms = mt;
 				}
+				cuts[i][i+k+1] = ms;
 			}
-			seg[k] = ms;
-			ans += ms;
-			for(i=k+1; i<c2-1; ++i) seg[i]=seg[i+1];
-			--c2;
 		}
-		printf("The minimum cutting is %d.\n", ans);
+
+		printf("The minimum cutting is %d.\n", cuts[0][n]);
 
 	}
 }
